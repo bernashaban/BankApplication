c762599aad893ff8d699f6ae502eca02c71c9cf8
Berna <bebozcan@gmail.com>
2022-04-24 14:39:46 +0300

    Account Repository added

--- src/main/java/application/dao/impl/AccountRepository.java
+++ src/main/java/application/dao/impl/AccountRepository.java
@@ -1,4 +1,151 @@
 package application.dao.impl;
 
-public class AccountRepository {
+import application.dao.Repository;
+import application.exception.EntityPersistenceException;
+import application.model.Account;
+import application.model.Client;
+import application.model.CurrencyType;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class AccountRepository extends AbstractRepository<Integer, Account> implements Repository<Integer, Account> {
+    public static final String SELECT_ALL_ACCOUNTS = "select * from `account`;";
+    public static final String INSERT_NEW_ACCOUNT = "insert into `account` (`account_number`, `interest`, `balance`, `client_id`, `currency_id`) values (?, ?, ?, ?, ?);";
+    public static final String SELECT_ACCOUNT_BY_ID ="select * from `account` where idaccount = ?;";
+    public static final String UPDATE_ACCOUNT_BY_ID="update `account` set `account_number` = ?, `interest` = ?, `balance` = ?, `client_id` = ?, `currency_id` = ? where idaccount = ?;";
+    public static final String DELETE_ACCOUNT_BY_ID="delete from `account` where idaccount = ?;";
+    private Connection connection;
+
+    public AccountRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public Collection<Account> findAll() {
+        try(var stmt = connection.prepareStatement(SELECT_ALL_ACCOUNTS)) {
+            var rs = stmt.executeQuery();
+            return toAccount(rs);
+        } catch (SQLException ex) {
+            System.out.println("Error creating connection to DB");
+            throw new EntityPersistenceException("Error executing SQL query: " + SELECT_ALL_ACCOUNTS, ex);
+        }
+    }
+
+    @Override
+    public Account findById(Integer id) {
+        var accounts = findAll();
+        try (var stmt = connection.prepareStatement(SELECT_ACCOUNT_BY_ID)) {
+            stmt.setInt(1, id);
+            var rs = stmt.executeQuery();
+            for (Account account : accounts) {
+                Integer currentId = account.getId();
+                if(currentId.equals(id)){
+                    return account;
+                }
+            }
+            return null;
+        } catch (SQLException ex) {
+            System.out.println("Error creating connection to DB");
+            throw new EntityPersistenceException("Error executing SQL query: " + SELECT_ALL_ACCOUNTS, ex);
+        }
+    }
+
+    @Override
+    public Account create(Account entity) {
+        try (var stmt = connection.prepareStatement(INSERT_NEW_ACCOUNT, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, entity.getAccountNum());
+            stmt.setDouble(2, entity.getInterest());
+            stmt.setDouble(3, entity.getBalance());
+            stmt.setInt(4, entity.getClient().getId());
+            stmt.setInt(5, entity.getCurrencyType().getId());
+            connection.setAutoCommit(false);
+            var affectedRows = stmt.executeUpdate();
+            connection.commit();
+            connection.setAutoCommit(true);
+            if (affectedRows == 0) {
+                throw new EntityPersistenceException("Creating appointment failed, no rows affected.");
+            }
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    entity.setId(generatedKeys.getInt(1));
+                    return entity;
+                } else {
+                    throw new EntityPersistenceException("Creating appointment failed, no ID obtained.");
+                }
+            }
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                throw new EntityPersistenceException("Error rolling back SQL query: " + SELECT_ALL_ACCOUNTS, ex);
+            }
+            System.out.println("Error creating connection to DB");
+            throw new EntityPersistenceException("Error executing SQL query: " + SELECT_ALL_ACCOUNTS, ex);
+        }
+    }
+
+    @Override
+    public Account update(Account entity)  {
+        var old = findById(entity.getId());
+        try {
+            var stmt = connection.prepareStatement(UPDATE_ACCOUNT_BY_ID);
+            stmt.setInt(1, entity.getAccountNum());
+            stmt.setDouble(2, entity.getInterest());
+            stmt.setDouble(3, entity.getBalance());
+            stmt.setInt(4, entity.getClient().getId());
+            stmt.setInt(5, entity.getCurrencyType().getId());
+            stmt.setInt(6, old.getId());
+
+            connection.setAutoCommit(false);
+            var affectedRows = stmt.executeUpdate();
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            System.out.println("Error creating connection to DB");
+            throw new EntityPersistenceException("Error executing SQL query: " + SELECT_ALL_ACCOUNTS, e);
+        }
+        return old;
+    }
+
+    @Override
+    public Account deleteById(Integer id)  {
+        var account = findById(id);
+        try (var stmt = connection.prepareStatement(DELETE_ACCOUNT_BY_ID)) {
+            stmt.setInt(1, id);
+            connection.setAutoCommit(false);
+            var affectedRows = stmt.executeUpdate();
+            connection.commit();
+            connection.setAutoCommit(true);
+
+        } catch (SQLException ex) {
+            System.out.println("Error creating connection to DB");
+            throw new EntityPersistenceException("Error executing SQL query: " + SELECT_ALL_ACCOUNTS, ex);
+        }
+        return account;
+    }
+
+    public List<Account> toAccount(ResultSet rs) throws SQLException {
+        List<Account> results = new ArrayList<>();
+        while (rs.next()) {
+            results.add(new Account(
+                    rs.getInt(1),
+                    rs.getInt(2),//account_number
+                    rs.getDouble(3),//interest
+                    rs.getDouble(4),//balance
+                    new Client(rs.getInt(5)),
+                    new CurrencyType()
+                    //clientService.findById(rs.getInt(5))  //client_id
+                    //currencyService.findById(rs.getInt(6)) //currency_id
+                    //userRepository.findById(rs.getLong(2)),
+
+            ));
+        }
+        return results;
+    }
 }
