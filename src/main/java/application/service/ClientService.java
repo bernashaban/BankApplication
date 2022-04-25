0730a82f1a66ef64f0fa1d21f72cb16131c73891
Berna <bebozcan@gmail.com>
2022-04-25 23:09:39 +0300

    Client Service added

---
+++ src/main/java/application/service/ClientService.java
@@ -0,0 +1,39 @@
+package application.service;
+
+import application.dao.impl.ClientRepository;
+import application.model.Client;
+
+import java.util.Collection;
+
+public class ClientService implements Service<Client> {
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public Collection<Client> getAll() {
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public Client getById(Integer id) {
+        return clientRepository.findById(id);
+    }
+
+    @Override
+    public Client add(Client entity) {
+        return clientRepository.create(entity);
+    }
+
+    @Override
+    public Client update(Client entity) {
+        return clientRepository.update(entity);
+    }
+
+    @Override
+    public Client delete(Integer id) {
+        return clientRepository.deleteById(id);
+    }
+}
