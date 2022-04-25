d623c3997896de3e4dad56e30c892d2b6746e119
Berna <bebozcan@gmail.com>
2022-04-25 23:17:03 +0300

    Transaction Service added

---
+++ src/main/java/application/service/TransactionService.java
@@ -0,0 +1,39 @@
+package application.service;
+
+import application.dao.impl.TransactionRepository;
+import application.model.Transaction;
+
+import java.util.Collection;
+
+public class TransactionService implements Service<Transaction> {
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    @Override
+    public Collection<Transaction> getAll() {
+        return transactionRepository.findAll();
+    }
+
+    @Override
+    public Transaction getById(Integer id) {
+        return transactionRepository.findById(id);
+    }
+
+    @Override
+    public Transaction add(Transaction entity) {
+        return transactionRepository.create(entity);
+    }
+
+    @Override
+    public Transaction update(Transaction entity) {
+        return transactionRepository.update(entity);
+    }
+
+    @Override
+    public Transaction delete(Integer id) {
+        return transactionRepository.deleteById(id);
+    }
+}
