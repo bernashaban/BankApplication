c96a486db336156c849fb15a4ade0822ec5bcc38
Berna <bebozcan@gmail.com>
2022-05-07 14:44:50 +0300

    Account Service modified

--- src/main/java/application/service/AccountService.java
+++ src/main/java/application/service/AccountService.java
@@ -2,10 +2,12 @@ package application.service;
 
 import application.dao.impl.AccountRepository;
 import application.model.Account;
+import application.model.Client;
+import application.model.CurrencyType;
 
 import java.util.Collection;
 
-public class AccountService implements Service<Account>{
+public class AccountService implements Service<Account> {
     private final AccountRepository accountRepository;
 
     public AccountService(AccountRepository accountRepository) {
@@ -35,5 +37,12 @@ public class AccountService implements Service<Account>{
     @Override
     public Account delete(Integer id) {
         return accountRepository.deleteById(id);
+    }
+
+    public Collection<Account> getClientAccounts(Client client) {
+        return accountRepository.findClientAccounts(client);
+    }
+    public Collection<Account> getClientAccountsByCurrency(Client client, CurrencyType currencyType) {
+        return accountRepository.findClientAccountsByCurrency(client, currencyType);
     }
 }
