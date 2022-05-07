2e65503a25e87ab6e9ce764d407f8163bd8c7415
Berna <bebozcan@gmail.com>
2022-05-07 14:54:55 +0300

    Main Controller added

---
+++ src/main/java/application/controller/MainController.java
@@ -0,0 +1,43 @@
+package application.controller;
+
+import application.service.*;
+import application.view.Menu;
+
+import java.util.List;
+
+public class MainController {
+
+    private final AccountService accountService;
+    private final ClientService clientService;
+    private final CurrencyService currencyService;
+    private final EmployeeService employeeService;
+    private final PositionService positionService;
+    private final TransactionService transactionService;
+    private final TransTypeService transTypeService;
+
+    public MainController(AccountService accountService, ClientService clientService, CurrencyService currencyService, EmployeeService employeeService, PositionService positionService, TransactionService transactionService, TransTypeService transTypeService) {
+        this.accountService = accountService;
+        this.clientService = clientService;
+        this.currencyService = currencyService;
+        this.employeeService = employeeService;
+        this.positionService = positionService;
+        this.transactionService = transactionService;
+        this.transTypeService = transTypeService;
+    }
+
+    public void init() {
+        var menu = new Menu("Main Menu", List.of(
+                new Menu.Option("Log in", () -> {
+                    LogInController logInController = new LogInController(clientService, employeeService, accountService, transactionService, currencyService, transTypeService);
+                    logInController.init();
+                    return ("");
+                }),
+                new Menu.Option("Register", () -> {
+                    RegisterController registerController = new RegisterController(clientService, employeeService);
+                    registerController.init();
+                    return ("");
+                })
+        ));
+        menu.show();
+    }
+}
