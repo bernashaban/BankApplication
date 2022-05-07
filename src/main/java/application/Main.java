f22bf9f068d7cc0b3325306e386a3271a58ef7cc
Berna <bebozcan@gmail.com>
2022-05-07 14:51:05 +0300

    Main class added

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,11 +1,16 @@
 package application;
 
+import application.controller.MainController;
+import application.dao.DaoFactory;
+import application.dao.impl.*;
+import application.service.*;
 import application.util.Jdbc;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Currency;
 import java.util.Properties;
 
 import static application.util.JdbcUtils.closeConnection;
@@ -27,10 +32,38 @@ public class Main {
             System.out.println("Error creating connection with the database.");
         }
 
+
+        DaoFactory daoFactory = new DaoFactoryImpl();
+
+        ClientRepository clientRepository = daoFactory.createClientRepository(conn);
+        ClientService clientService = new ClientService(clientRepository);
+
+        CurrencyRepository currencyRepository = daoFactory.createCurrencyRepository(conn);
+        CurrencyService currencyService = new CurrencyService(currencyRepository);
+
+        PositionRepository positionRepository = daoFactory.createPositionRepository(conn);
+        PositionService positionService = new PositionService(positionRepository);
+
+        TransTypeRepository transTypeRepository = daoFactory.createTransTypeRepository(conn);
+        TransTypeService transTypeService =new TransTypeService(transTypeRepository);
+
+        AccountRepository accountRepository = daoFactory.createAccountRepository(conn,clientService, currencyService);
+        AccountService accountService = new AccountService(accountRepository);
+
+        EmployeeRepository employeeRepository = daoFactory.createEmployeeRepository(conn,positionService);
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+
+        TransactionRepository transactionRepository=daoFactory.createTransactionRepository(conn, transTypeService,employeeService,accountService);
+        TransactionService transactionService = new TransactionService(transactionRepository);
+
+        var mainController = new MainController(accountService,clientService,currencyService,employeeService,positionService,transactionService,transTypeService);
+        mainController.init();
+
         try {
             closeConnection(conn);
         } catch (SQLException e) {
             System.out.println("Error while closing the connection with database.");
         }
+
     }
 }
