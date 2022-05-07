df987e0ee938e1ce51eb7373041562e5c84f7642
Berna <bebozcan@gmail.com>
2022-05-07 14:46:59 +0300

    DAO Factory interface added

--- src/main/java/application/dao/DaoFactory.java
+++ src/main/java/application/dao/DaoFactory.java
@@ -1,4 +1,17 @@
 package application.dao;
 
+import application.dao.impl.*;
+import application.service.*;
+
+import java.sql.Connection;
+
 public interface DaoFactory {
+    AccountRepository createAccountRepository(Connection connection,ClientService clientService, CurrencyService currencyService);
+    ClientRepository createClientRepository(Connection connection);
+    CurrencyRepository createCurrencyRepository(Connection connection);
+    EmployeeRepository createEmployeeRepository(Connection connection, PositionService positionService);
+    PositionRepository createPositionRepository(Connection connection);
+    TransactionRepository createTransactionRepository(Connection connection, TransTypeService transTypeService, EmployeeService employeeService, AccountService accountService);
+    TransTypeRepository createTransTypeRepository(Connection connection);
+
 }
