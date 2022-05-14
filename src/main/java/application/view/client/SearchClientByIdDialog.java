98c55c2ba040f32003389a75ec607d90a624d560
Berna <bebozcan@gmail.com>
2022-05-14 15:51:18 +0300

    Search Client By ID dialog added

---
+++ src/main/java/application/view/client/SearchClientByIdDialog.java
@@ -0,0 +1,37 @@
+package application.view.client;
+
+import application.exception.NonExistingEntityException;
+import application.model.Client;
+import application.service.ClientService;
+import application.view.EntityDialog;
+
+import java.util.Scanner;
+
+public class SearchClientByIdDialog implements EntityDialog<Client> {
+    public static Scanner scanner = new Scanner(System.in);
+    private ClientService clientService;
+
+    public SearchClientByIdDialog(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @Override
+    public Client input() throws NonExistingEntityException {
+        Client client = null;
+        while (client == null) {
+            System.out.println("ID на клиент: ");
+            var answer = scanner.nextLine();
+            int id = 0;
+            try {
+                id = Integer.parseInt(answer);
+            } catch (NumberFormatException e) {
+                System.out.println("Невалиден формат на ID. Моля въведете число.");
+            }
+            client =  clientService.getById(id);
+            if(client==null){
+                System.out.println("Клиент с такова ID не съществува!");
+            }
+        }
+        return client;
+    }
+}
